2e1e728141653a02710ce00f0cae6e9971144fd1
Mwelwa Nkuta <mwelwankuta@gmail.com>
2024-04-13 15:01:41 +0200

    seo: added meta description

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -35,12 +35,13 @@ export const Home = () => {
           <h1 className="font-black text-6xl lg:text-8xl hidden lg:flex">👋🏽</h1>
           <img
             src="https://media.licdn.com/dms/image/D4D03AQGEcWcZCYbSKA/profile-displayphoto-shrink_200_200/0/1705765898593?e=1718236800&v=beta&t=2Ag099kdHQ2fkRD7SPf2kVyCNAKkjDIRBPDTz_C9xpg"
-            alt=""
+            alt="Picture of mwelwa"
             className="rounded-full h-[70px] w-[70px] object-cover self-end lg:hidden"
           />
         </div>
         <p className="text-slate-900">
-          Hello, I'm Mwelwa Nkuta, A passionate Software Engineer with 4 years of experience
+          Hello, I'm Mwelwa Nkuta, A passionate Software Engineer with 4 years
+          of experience
         </p>
 
         <div className="flex items-center gap-9">
@@ -62,47 +63,87 @@ export const Home = () => {
 
         <div className="flex flex-wrap items-center gap-4">
           <a href="https://react.dev/" title="React.js">
-            <img src={reactjs} className="size-11 object-contain" />
+            <img
+              src={reactjs}
+              className="size-11 object-contain"
+              alt="react.js logo"
+            />
           </a>
 
           <a href="https://www.typescriptlang.org/" title="TypeScript">
-            <img src={typescript} className="size-11 object-contain" />
+            <img
+              src={typescript}
+              className="size-11 object-contain"
+              alt="typescript logo"
+            />
           </a>
 
           <a href="https://go.dev/" title="Go Lang">
-            <img src={go} className="size-11 object-contain" />
+            <img
+              src={go}
+              className="size-11 object-contain"
+              alt="golang logo"
+            />
           </a>
 
           <a href="https://reactnative.dev/" title="React Native">
-            <img src={reactnative} className="w-[80px] object-contain" />
+            <img
+              src={reactnative}
+              className="w-[80px] object-contain"
+              alt="react native logo"
+            />
           </a>
 
           <a href="https://electronjs.org/" title="Electron.js">
-            <img src={electron} className="size-11 object-contain" />
+            <img
+              src={electron}
+              className="size-11 object-contain"
+              alt="electron.js logo"
+            />
           </a>
 
           <a href="https://nodejs.org" title="Node.js">
-            <img src={nodejs} className="size-11 object-contain" />
+            <img
+              src={nodejs}
+              className="size-11 object-contain"
+              alt="node.js logo"
+            />
           </a>
 
           <a href="https://graphql.org/" title="GraphQL">
-            <img src={graphql} className="size-11 object-contain" />
+            <img
+              src={graphql}
+              className="size-11 object-contain"
+              alt="graphql logo"
+            />
           </a>
 
           <a href="https://serverless.com/" title="Serverless">
-            <img src={serverless} className="size-11 object-contain" />
+            <img
+              src={serverless}
+              className="size-11 object-contain"
+              alt="serverless logo"
+            />
           </a>
 
           <a href="https://www.mongodb.com/" title="Mongo DB">
-            <img src={mongodb} className="size-11 object-contain" />
+            <img
+              src={mongodb}
+              className="size-11 object-contain"
+              alt="mongodb logo"
+            />
           </a>
 
           <a href="https://www.mysql.com/" title="MySQL">
-            <img src={mysql} className="size-11 object-contain" />
+            <img
+              src={mysql}
+              className="size-11 object-contain"
+              alt="mysql logo"
+            />
           </a>
 
           <a href="https://aws.amazon.com/" title="Amazon Web Services">
-            <img src={aws} className="size-11 object-contain" />
+            <img src={aws} className="size-11 object-contain" alt="aws logo" />
           </a>
         </div>
 
@@ -117,7 +158,7 @@ export const Home = () => {
       <div className="hidden lg:flex">
         <img
           src="https://media.licdn.com/dms/image/D4D03AQGEcWcZCYbSKA/profile-displayphoto-shrink_200_200/0/1705765898593?e=1718236800&v=beta&t=2Ag099kdHQ2fkRD7SPf2kVyCNAKkjDIRBPDTz_C9xpg"
-          alt=""
+          alt="picture of Mwelwa"
           className="rounded-full h-[300px] w-[300px] object-cover"
         />
       </div>
