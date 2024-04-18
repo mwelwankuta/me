6df1b57c180e4fc4daaac8e08aae701bee9b17e3
Mwelwa Nkuta <mwelwankuta@gmail.com>
2024-04-18 17:08:28 +0200

    style: centreted footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="container mx-auto bg-white blur-md">
+    <div className="container mx-auto bg-white text-center p-2 py-4 mt-10">
       Copyright &copy; Mwelwa Nkuta. All rights reserved
     </div>
   );
